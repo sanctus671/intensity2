b07eabb945d83eca0d033ea86ae69e17734fcb27
sanctus671 <taylor@taylorhamling.com>
2018-01-16 14:22:17 +1300

    .

--- www/build/main.js
+++ www/build/main.js
@@ -1233,14 +1233,13 @@ var TimerService = (function () {
                         });
                     }
                     else if (_this.countdownTimerProperties.started && _this.countdownTimerProperties.repeat) {
-                        for (var x = 0; x < 100; x++) {
-                            console.log(new Date(new Date().getTime() + _this.countdownTimer));
+                        for (var x = 1; x < 10; x++) {
+                            new Date(new Date().getTime() + (_this.countdownTimer + (_this.countdownTimerProperties.time * x)));
                             console.log(x + 2);
                             _this.localNotifications.schedule({
-                                id: x + 2,
+                                id: 2,
                                 title: 'Timer has completed (will repeat)',
-                                at: new Date(new Date().getTime() + _this.countdownTimer + (_this.countdownTimerProperties.time * x)),
-                                sound: _this.platform.is("android") ? 'file://assets/audio/timer.mp3' : 'file://assets/audio/timer.caf',
+                                at: new Date(new Date().getTime() + (_this.countdownTimer + (_this.countdownTimerProperties.time * x)))
                             });
                         }
                     }
@@ -1339,9 +1338,10 @@ var TimerService = (function () {
     };
     TimerService = __decorate([
         Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["Injectable"])(),
-        __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_2__ionic_native_native_audio__["a" /* NativeAudio */], __WEBPACK_IMPORTED_MODULE_3_ionic_angular__["d" /* Events */], __WEBPACK_IMPORTED_MODULE_3_ionic_angular__["m" /* Platform */], __WEBPACK_IMPORTED_MODULE_4__ionic_native_local_notifications__["a" /* LocalNotifications */]])
+        __metadata("design:paramtypes", [typeof (_a = typeof __WEBPACK_IMPORTED_MODULE_2__ionic_native_native_audio__["a" /* NativeAudio */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_2__ionic_native_native_audio__["a" /* NativeAudio */]) === "function" && _a || Object, typeof (_b = typeof __WEBPACK_IMPORTED_MODULE_3_ionic_angular__["d" /* Events */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_3_ionic_angular__["d" /* Events */]) === "function" && _b || Object, typeof (_c = typeof __WEBPACK_IMPORTED_MODULE_3_ionic_angular__["m" /* Platform */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_3_ionic_angular__["m" /* Platform */]) === "function" && _c || Object, typeof (_d = typeof __WEBPACK_IMPORTED_MODULE_4__ionic_native_local_notifications__["a" /* LocalNotifications */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_4__ionic_native_local_notifications__["a" /* LocalNotifications */]) === "function" && _d || Object])
     ], TimerService);
     return TimerService;
+    var _a, _b, _c, _d;
 }());
 
 //# sourceMappingURL=timer.js.map
